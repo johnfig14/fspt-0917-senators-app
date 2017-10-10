c7871dfb5c0a0f21f1c210a147a31332e049cac3
Danny Warren <dannydwarren@gmail.com>
2017-10-09 18:30:07 -0600

    Init Commit

---
+++ src/Senators.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SenatorsData from './data/senators.json'
+
+class Senators extends Component {
+    render() {
+        return (
+            <div>Show the Sentors here...</div>
+        )
+    }
+}  
+
+export default Senators;
